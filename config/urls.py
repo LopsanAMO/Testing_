328ba296460aa836dac8be956b9a358606d2740f
LopsanAMO <makiki789@gmail.com>
2018-01-29 14:47:30 -0600

    adding static root

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib.auth import views as auth
 from api import urls as APIV1Urls
 from users import urls as UserUrls
@@ -12,4 +15,21 @@ urlpatterns = [
     path('', Home.as_view()),
     path('', include('social_django.urls', namespace='social')),
     path('logout/', auth.logout, {'next_page': '/'}, name='logout'),
+]
+
+urlpatterns += [
+    path(
+        'media/<str:path>/',
+        serve,
+        {
+            'document_root': settings.MEDIA_ROOT,
+        }
+    ),
+    path(
+        'static/<str:path>/',
+        serve,
+        {
+            'document_root': settings.STATIC_ROOT
+        }
+    )
 ]
